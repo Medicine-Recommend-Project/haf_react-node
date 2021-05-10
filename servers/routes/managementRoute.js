76c6588c0e8a29de28befae9fc145fd822ebfc03
hyeji <gpwl9610@gmail.com>
2021-05-11 05:21:37 +0900

    상품 타입컬럼 추가

--- servers/routes/managementRoute.js
+++ servers/routes/managementRoute.js
@@ -37,8 +37,8 @@ router.post('/image', (req, res) =>{
 router.post('/addProduct', (req, res) =>{
 
 // 받아온 정보들을 DB에 넣어준다
-    sqlQuery = " INSERT INTO product(pcode, pname, description, price, continents, images) VALUES (CREATE_PCODE_FC(), ?, ?, ?, ?, ?) ";
-    data = [req.body.pname, req.body.description, req.body.price ,req.body.continents, req.body.images ];
+    sqlQuery = " INSERT INTO product(pcode, type, pname, description, price, continents, images) VALUES (CREATE_PCODE_FC(), ?, ?, ?, ?, ?, ?) ";
+    data = [req.body.pname, req.body.type, req.body.description, req.body.price ,req.body.continents, req.body.images ];
     // logger.info('여기서 이미지 값 내용은? ' + req.body.images);
     sql = db.query(sqlQuery, data, (err, row) => {
         //insert 가 정상적으로 적용되었는지 판단하는 방법은 result.affectedRows를 활용합니다. (update, delete도 동일)
