6b48cbd6963d4970cd5eb188a48479a2b3b369c9
kim_hyeji <gpwl9610@gmail.com>
2021-09-24 17:09:14 +0900

    backend Error Handling 추가 및 proxy 설정 변경(새로고침 버그 해결)

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -5,7 +5,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 // ★ 새로 경로 추가할 때 마다 []여기 배열 안에 '/경로' 추가해줘야함 ★
 module.exports = function (app){
     app.use(
-        createProxyMiddleware(['/api','/customer','/product','/order','/board','/user','/uploads', '/management'], {
+        createProxyMiddleware(['/api','/uploads'], {
             target: 'http://localhost:3001',
             changeOrigin: true
         })
